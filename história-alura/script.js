48152ddc2221705eccf8a71a15247f91f1a2ae8e
Lucas Quinteiro Campos <136658353+Lucasqc04@users.noreply.github.com>
2024-08-18 18:26:29 -0300

    final commit

---
+++ história-alura/script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('.btn-proximo').forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    });
+});
+
+document.querySelectorAll('.btn-reiniciar').forEach(button => {
+    button.addEventListener('click', function() {
+        location.reload();  
+    });
+});
